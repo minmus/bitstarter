d6a01189de0db2be6e7cecb2b0e387ecc32879eb
minmus <minmus@gmail.com>
2013-07-09 12:19:18 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 buf = new Buffer(fs.readFileSync('index.html'));
 
 app.get('/', function(request, response) {
-  response.send(buf);
+  response.send(bu.toString);
 });
 
 var port = process.env.PORT || 5000;
